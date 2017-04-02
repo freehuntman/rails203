df9f9626a60eadfaad7bd1b3c20121405c517838
吴海铭 <freehuntman@gmail.com>
2017-04-02 23:46:58 +0800

    implement post's Edit & Delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 
+  before_action :authenticate_user!, :only => [:new, :create]
   def new
     @group = Group.find(params[:group_id]) #还不熟
     @post = Post.new
@@ -17,6 +18,33 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: 'Update post success'
+    else
+      render :edit
+    end
+  end
+
+
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to account_posts_path, alert: 'Post deleted'
+  end
+
+
 
   private
 
