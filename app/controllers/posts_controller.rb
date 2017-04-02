9cc736e12fd931fb263d6e85ed4a3d76d161b5e3
吴海铭 <freehuntman@gmail.com>
2017-04-02 16:40:11 +0800

    implement post create function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+
+  def new
+    @group = Group.find(params[:group_id]) #还不熟
+    @post = Post.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group #应该是把group的参数赋予post的group属性
+    @post.user = current_user #应该是把current_user参数赋予post的user属性
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content) #还不熟
+  end
+
 end
